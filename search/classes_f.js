d19e55da763343698de9ad2d37d1939999fef6b2
TimSchneider42 <TimSchneider42@users.noreply.github.com>
2024-05-27 19:09:49 +0000

    deploy: 0cecf0899105ee89ee099b2b254fbe221ed4b5ff

--- search/classes_f.js
+++ search/classes_f.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['waypoint_331',['Waypoint',['../structfranky_1_1_waypoint.html',1,'franky']]],
   ['waypointmotion_332',['WaypointMotion',['../classfranky_1_1_waypoint_motion.html',1,'franky']]],
-  ['waypointmotion_3c_20franka_3a_3acartesianpose_2c_20franky_3a_3arobotpose_20_3e_333',['WaypointMotion&lt; franka::CartesianPose, franky::RobotPose &gt;',['../classfranky_1_1_waypoint_motion.html',1,'franky']]],
-  ['waypointmotion_3c_20franka_3a_3acartesianpose_2c_20robotpose_20_3e_334',['WaypointMotion&lt; franka::CartesianPose, RobotPose &gt;',['../classfranky_1_1_waypoint_motion.html',1,'franky']]],
+  ['waypointmotion_3c_20franka_3a_3acartesianpose_2c_20cartesianstate_20_3e_333',['WaypointMotion&lt; franka::CartesianPose, CartesianState &gt;',['../classfranky_1_1_waypoint_motion.html',1,'franky']]],
+  ['waypointmotion_3c_20franka_3a_3acartesianpose_2c_20franky_3a_3acartesianstate_20_3e_334',['WaypointMotion&lt; franka::CartesianPose, franky::CartesianState &gt;',['../classfranky_1_1_waypoint_motion.html',1,'franky']]],
   ['waypointmotion_3c_20franka_3a_3ajointpositions_2c_20vector7d_20_3e_335',['WaypointMotion&lt; franka::JointPositions, Vector7d &gt;',['../classfranky_1_1_waypoint_motion.html',1,'franky']]]
 ];
