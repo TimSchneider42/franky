ad6412e5eebbcfc4bbe47c24e02137eb68d0c73b
TimSchneider42 <TimSchneider42@users.noreply.github.com>
2024-05-27 19:49:34 +0000

    deploy: 6be576fc954f0f371c7dd87b4eab2b27eab9bbf3

--- search/all_16.js
+++ search/all_16.js
@@ -5,7 +5,7 @@ var searchData=
   ['waypointmotion_280',['WaypointMotion',['../classfranky_1_1_waypoint_motion.html',1,'franky::WaypointMotion&lt; ControlSignalType, TargetType &gt;'],['../classfranky_1_1_waypoint_motion.html#a988a4be1ead6c7231da77169316c67eb',1,'franky::WaypointMotion::WaypointMotion()']]],
   ['waypointmotion_3c_20franka_3a_3acartesianpose_2c_20cartesianstate_20_3e_281',['WaypointMotion&lt; franka::CartesianPose, CartesianState &gt;',['../classfranky_1_1_waypoint_motion.html',1,'franky']]],
   ['waypointmotion_3c_20franka_3a_3acartesianpose_2c_20franky_3a_3acartesianstate_20_3e_282',['WaypointMotion&lt; franka::CartesianPose, franky::CartesianState &gt;',['../classfranky_1_1_waypoint_motion.html',1,'franky']]],
-  ['waypointmotion_3c_20franka_3a_3ajointpositions_2c_20vector7d_20_3e_283',['WaypointMotion&lt; franka::JointPositions, Vector7d &gt;',['../classfranky_1_1_waypoint_motion.html',1,'franky']]],
+  ['waypointmotion_3c_20franka_3a_3ajointpositions_2c_20jointstate_20_3e_283',['WaypointMotion&lt; franka::JointPositions, JointState &gt;',['../classfranky_1_1_waypoint_motion.html',1,'franky']]],
   ['width_284',['width',['../classfranky_1_1_gripper.html#a176c18c2ca98694075eea1085dc65a7e',1,'franky::Gripper']]],
   ['with_5felbow_5fposition_285',['with_elbow_position',['../classfranky_1_1_robot_pose.html#a0172a87f3b12aa7dc0482d46168a6c5f',1,'franky::RobotPose']]]
 ];
--- search/classes_f.js
+++ search/classes_f.js
@@ -4,5 +4,5 @@ var searchData=
   ['waypointmotion_332',['WaypointMotion',['../classfranky_1_1_waypoint_motion.html',1,'franky']]],
   ['waypointmotion_3c_20franka_3a_3acartesianpose_2c_20cartesianstate_20_3e_333',['WaypointMotion&lt; franka::CartesianPose, CartesianState &gt;',['../classfranky_1_1_waypoint_motion.html',1,'franky']]],
   ['waypointmotion_3c_20franka_3a_3acartesianpose_2c_20franky_3a_3acartesianstate_20_3e_334',['WaypointMotion&lt; franka::CartesianPose, franky::CartesianState &gt;',['../classfranky_1_1_waypoint_motion.html',1,'franky']]],
-  ['waypointmotion_3c_20franka_3a_3ajointpositions_2c_20vector7d_20_3e_335',['WaypointMotion&lt; franka::JointPositions, Vector7d &gt;',['../classfranky_1_1_waypoint_motion.html',1,'franky']]]
+  ['waypointmotion_3c_20franka_3a_3ajointpositions_2c_20jointstate_20_3e_335',['WaypointMotion&lt; franka::JointPositions, JointState &gt;',['../classfranky_1_1_waypoint_motion.html',1,'franky']]]
 ];
