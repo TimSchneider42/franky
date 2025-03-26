09aad5119ad38a9335589ea313af38a2812c7329
TimSchneider42 <TimSchneider42@users.noreply.github.com>
2025-03-26 00:30:42 +0000

    deploy: cef7e7c22235201a3c1997805428f1b29ffefd6f

--- search/functions_7.js
+++ search/functions_7.js
@@ -5,7 +5,7 @@ var searchData=
   ['joinmotion_2',['joinmotion',['../classfranky_1_1_robot.html#a2f48445583c522dd798e312bdbacf864',1,'franky::Robot::joinMotion()'],['../classfranky_1_1_robot.html#a114a614ee78f9b554dd9aaef8fe2a1d8',1,'franky::Robot::joinMotion(const std::chrono::duration&lt; Rep, Period &gt; &amp;timeout)']]],
   ['jointmotion_3',['JointMotion',['../classfranky_1_1_joint_motion.html#a0148b712398a4aa57f2fb6849907595c',1,'franky::JointMotion']]],
   ['jointstate_4',['jointstate',['../classfranky_1_1_joint_state.html#a58de323134a7b11599dafbcb3d9fcf1c',1,'franky::JointState::JointState(Vector7d position)'],['../classfranky_1_1_joint_state.html#ad4a1bcb1c1f08f59f160c64f05462c1b',1,'franky::JointState::JointState(Vector7d position, Vector7d velocity)'],['../classfranky_1_1_joint_state.html#a568e0e11672f5d9ad1f42b1d6cb9a576',1,'franky::JointState::JointState(const JointState &amp;)=default'],['../classfranky_1_1_joint_state.html#a330222fd0fea56aef1c1a7a6eb918e9b',1,'franky::JointState::JointState()=default']]],
-  ['jointvelocitymotion_5',['JointVelocityMotion',['../classfranky_1_1_joint_velocity_motion.html#a671e1f2d8e0a65bfceb5f1b6c4d81550',1,'franky::JointVelocityMotion']]],
+  ['jointvelocitymotion_5',['JointVelocityMotion',['../classfranky_1_1_joint_velocity_motion.html#ac6b66f806036e8a35354fe4483aa631a',1,'franky::JointVelocityMotion']]],
   ['jointvelocitywaypointmotion_6',['JointVelocityWaypointMotion',['../classfranky_1_1_joint_velocity_waypoint_motion.html#a42129c9f2d942dfe5200425f1eaed709',1,'franky::JointVelocityWaypointMotion']]],
   ['jointwaypointmotion_7',['JointWaypointMotion',['../classfranky_1_1_joint_waypoint_motion.html#a6f0d8ac1afdbf632209a981e8d48ba36',1,'franky::JointWaypointMotion']]]
 ];
