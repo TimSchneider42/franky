6a51b8b25e4cad493825fb87ab3add9a2dde56b9
TimSchneider42 <TimSchneider42@users.noreply.github.com>
2025-03-27 09:11:26 +0000

    deploy: ab15a1ed0ea4789138d8b85dc3e1a6fc2fa042b0

--- search/variables_9.js
+++ search/variables_9.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['path_0',['path',['../structfranky_1_1_trajectory.html#a2aee2a16dab2a1e98cf105c86128c8d9',1,'franky::Trajectory']]]
+  ['path_0',['path',['../structfranky_1_1_trajectory.html#a2aee2a16dab2a1e98cf105c86128c8d9',1,'franky::Trajectory']]],
+  ['position_1',['position',['../structfranky_1_1_joint_state.html#a6e1aa5bff87c1be7b394c76c6168d41b',1,'franky::JointState']]]
 ];
--- search/variables_e.js
+++ search/variables_e.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['value_0',['value',['../structfranky_1_1_kinematics_1_1_null_space_handling.html#a6896c0ba84d77e443908e15ff5b24f0a',1,'franky::Kinematics::NullSpaceHandling']]]
+  ['value_0',['value',['../structfranky_1_1_kinematics_1_1_null_space_handling.html#a6896c0ba84d77e443908e15ff5b24f0a',1,'franky::Kinematics::NullSpaceHandling']]],
+  ['velocity_1',['velocity',['../structfranky_1_1_joint_state.html#a0af8c680b67c5bc7ade9a29d18f3063e',1,'franky::JointState']]]
 ];
