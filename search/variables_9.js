07cbaf1d613cd3772f4e3c6f5a1e44e0ecfae2ee
TimSchneider42 <TimSchneider42@users.noreply.github.com>
2025-04-02 16:49:28 +0000

    deploy: 8d959f047e1c6f56c22893ebdff9eb1a450c8a65

--- search/variables_9.js
+++ search/variables_9.js
@@ -4,5 +4,6 @@ var searchData=
   ['m_5fload_1',['m_load',['../structfranky_1_1_robot_state.html#ac7f17c8d7d1bb49658c872904caa8233',1,'franky::RobotState']]],
   ['m_5ftotal_2',['m_total',['../structfranky_1_1_robot_state.html#a8e8ddca4a286bfb33aa94bfbafc7666d',1,'franky::RobotState']]],
   ['max_3',['max',['../classfranky_1_1_dynamics_limit.html#a8ad7788915e12262221aa983c0588d62',1,'franky::DynamicsLimit']]],
-  ['minimum_5ftime_4',['minimum_time',['../structfranky_1_1_waypoint.html#ae0fed925318bbd85540a99b018efe0bf',1,'franky::Waypoint']]]
+  ['max_5ftotal_5fduration_4',['max_total_duration',['../structfranky_1_1_waypoint.html#a2a1eae68a4e9d3b9c54a0ff1fd6c9bb2',1,'franky::Waypoint']]],
+  ['minimum_5ftime_5',['minimum_time',['../structfranky_1_1_waypoint.html#ae0fed925318bbd85540a99b018efe0bf',1,'franky::Waypoint']]]
 ];
