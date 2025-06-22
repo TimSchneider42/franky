2d904e6fda8224d025054af5d5d022761f4fd364
Albert Li <alberthli@caltech.edu>
2025-06-22 15:04:19 -0700

    clean up if statement logic

--- franky/robot_web_session.py
+++ franky/robot_web_session.py
@@ -155,7 +155,7 @@ class RobotWebSession:
         force: bool = False,
         retry_with_force: bool = True,
     ):
-        if not self.has_control() or force:
+        if not self.has_control():
             res = self.send_api_request(
                 f"/admin/api/control-token/request{'?force' if force else ''}",
                 headers={"content-type": "application/json"},
@@ -176,7 +176,7 @@ class RobotWebSession:
                 time.sleep(max(0.0, min(1.0, wait_timeout - (time.time() - start))))
                 has_control = self.has_control()
             if not has_control:
-                if retry_with_force and not force:
+                if retry_with_force:
                     # use much longer timeout to allow user to physically press button
                     return self.take_control(
                         wait_timeout=30.0, force=True, retry_with_force=False
