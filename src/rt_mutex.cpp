d27192724184d557ab253c20f3d4a9ed8f69161c
Tim Schneider <tim@robot-learning.de>
2026-06-25 16:18:55 +0200

    Prevented potential double-free in rt_mutex.cpp

--- src/rt_mutex.cpp
+++ src/rt_mutex.cpp
@@ -13,9 +13,8 @@
 #endif
 
 namespace franky {
-
 // Common helper function to handle error checking for system calls
-void checkRes(int res, const std::string &msg) {
+void checkRes(int res, const std::string& msg) {
   if (res != 0) {
     std::stringstream ss;
     ss << msg << ": " << std::strerror(res);
@@ -31,7 +30,7 @@ void checkRes(int res, const std::string &msg) {
  *
  * @param mutex Mutex to patch.
  */
-void patchMutexRT(std::mutex &mutex) {
+void patchMutexRT(std::mutex& mutex) {
 #if defined(_WIN32) || defined(_WIN64)
   // Static variable to ensure warning is printed only once
   static bool warningPrinted = false;
@@ -47,15 +46,20 @@ void patchMutexRT(std::mutex &mutex) {
   // POSIX (pthread) solution
   pthread_mutexattr_t attr;
 
-  checkRes(pthread_mutex_destroy(mutex.native_handle()), "Failed to destroy native mutex.");
-  checkRes(pthread_mutexattr_init(&attr), "Failed to initialize pthread mutex attributes");
+  checkRes(pthread_mutexattr_init(&attr), "Failed to initialize pthread mutex attributes.");
   checkRes(
       pthread_mutexattr_setprotocol(&attr, PTHREAD_PRIO_INHERIT),
-      "Failed to enable priority inheritance for pthread mutex");
-  checkRes(pthread_mutex_init(mutex.native_handle(), &attr), "Failed to initialize mutex");
+      "Failed to enable priority inheritance for pthread mutex.");
+  checkRes(pthread_mutex_destroy(mutex.native_handle()), "Failed to destroy native mutex.");
+
+  auto success = pthread_mutex_init(mutex.native_handle(), &attr);
+  if (success != 0)
+    checkRes(
+        pthread_mutex_init(mutex.native_handle(), nullptr),
+        "Failed to re-initialize standard mutex after failure to initialize RT mutex.");
+  checkRes(success, "Failed to initialize RT mutex.");
 
   pthread_mutexattr_destroy(&attr);
 #endif
 }
-
 }  // namespace franky
