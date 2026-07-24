2f14e14bb2d4ea25dc49744ea065537f8ac278e3
Nick Walker <nick@nickwalker.us>
2026-07-24 14:59:13 -0400

    Fix a flipped sign in elbow integration
    This probably makes no difference either way, but the coding bots keep tripping over it when asked to review other changes...

--- src/motion/cartesian_velocity_waypoint_motion.cpp
+++ src/motion/cartesian_velocity_waypoint_motion.cpp
@@ -67,7 +67,7 @@ franka::CartesianVelocities CartesianVelocityWaypointMotion::getControlSignal(
     if (time_step_s > 0.0) {
       auto last_elbow_acc = 2 * (current_elbow_vel - last_elbow_vel_) / time_step_s - current_elbow_acc;
       auto elbow_jerk = (current_elbow_acc - last_elbow_acc) / time_step_s;
-      current_elbow_pos += current_elbow_vel * time_step_s + 0.5 * current_elbow_acc * std::pow(time_step_s, 2) +
+      current_elbow_pos += current_elbow_vel * time_step_s - 0.5 * current_elbow_acc * std::pow(time_step_s, 2) +
                            1.0 / 6.0 * elbow_jerk * std::pow(time_step_s, 3);
     }
 
